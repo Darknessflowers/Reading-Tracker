277b7ffd6ac27c3d20f75cf7a86aa413c318d2d5
Darknessflowers <darknessflowers@hotmail.co.uk>
2021-01-13 22:32:50 +1300

    Change year value to not be hardcoded.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
 let booksRead = document.querySelector('#booksRead');
 let booksGoal = document.querySelector('#booksGoal');
 let currentDate = Date.now();
-let endOfChallenge = new Date('December 31, 2021 23:59:59').getTime();
+let currentYear = new Date().getFullYear();
+let endOfChallenge = new Date(`December 31, ${currentYear} 23:59:59`).getTime();
 let oneDay = 1000 * 60 * 60 * 24;
 let daysLeft = Math.round((endOfChallenge - currentDate) / oneDay);
 let daysLeftDisplay = document.querySelector('#daysLeftDisplay');
